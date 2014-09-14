49ef6c70e71b7b0d7f036c8b0c0434726c08586d
youssefbenhssaien <youssefbenhssaien@gmail.com>
2014-09-14 16:35:17 +0100

    Revert "YAB MVC"
    This reverts commit 72c1f08b085e0782140df944ad7e4dd248610d31.

--- YAB MVC/include/load_include_files.php
+++
@@ -1,8 +0,0 @@
-<?php
-if ($dh = opendir(__DIR__)){
-	while (($file = readdir($dh)) !== false){
-		if(strcmp($file,".")!=0 && strcmp($file,"..")!=0  && strcmp($file,basename(__FILE__))!=0 && is_file(Config::$ROOT_INCLUDE."/".$file))require Config::$ROOT_INCLUDE."/".$file;
-	}
-	closedir($dh);
-}
-?>
--- YAB MVC/public/panel/fr/includes/left.header.php
+++
@@ -1,5 +0,0 @@
-<div class="leftpanel">
-	<div class="leftmenu">
-		MENUE ICI !
-	</div>
-</div>
